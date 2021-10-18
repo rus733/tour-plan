53cf6894ed453c2c6ae67ce7169326f6b8494988
rus733 <9282716617@mail.ru>
2021-10-18 20:52:48 +0300

    create : mobile menu

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
-  
+
   // Navigation arrows
   navigation: {
     nextEl: ".hotel-slider__button--next",
     prevEl: ".hotel-slider__button--prev",
   },
-  effect: 'cowerflow',
+  effect: "cowerflow",
 });
 const reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
-  
+
   // Navigation arrows
   navigation: {
     nextEl: ".reviews-slider__button--next",
     prevEl: ".reviews-slider__button--prev",
   },
-}); 
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
